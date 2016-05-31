17c088d4b1a4bbd77a19773196e5ae53e6314a72
ckubec <ckubec@gmail.com>
2016-05-30 17:29:29 -0700

    Login works properly with database

--- app/src/main/java/authenticate/LoginActivity.java
+++ app/src/main/java/authenticate/LoginActivity.java
@@ -5,11 +5,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import ckubec.tacoma.uw.edu.carparker.MainActivity;
 import ckubec.tacoma.uw.edu.carparker.R;
 
@@ -17,6 +23,7 @@ import ckubec.tacoma.uw.edu.carparker.R;
  * Created by ckubec on 5/28/16.
  */
 public class LoginActivity extends AppCompatActivity {
+    private String LOGGING = "http://cssgate.insttech.washington.edu/~ckubec/Android/login.php?";
     private SharedPreferences mSharedPreferences;
 
     /**
@@ -40,12 +47,6 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(i);
             finish();
         }
-
-
-        /*getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container, new LoginFragment())
-                .commit();*/
-
     }
 
     /**
@@ -58,6 +59,12 @@ public class LoginActivity extends AppCompatActivity {
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        SharedPreferences sharedPreferences =
+                getSharedPreferences(getString(R.string.LOGIN_PREFS), Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString(getString(R.string.login), userId)
+                .commit();
+
         if (networkInfo != null && networkInfo.isConnected()) {
             //Check if the login and password are valid
             //new LoginTask().execute(url);
@@ -65,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
                 OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
                         openFileOutput(getString(R.string.LOGIN_FILE)
                                 , Context.MODE_PRIVATE));
-                outputStreamWriter.write("email = " + userId + ";");
+                outputStreamWriter.write("user = " + userId + ";");
                 outputStreamWriter.write("password = " + pwd);
                 outputStreamWriter.close();
             } catch (Exception e) {
@@ -82,10 +89,72 @@ public class LoginActivity extends AppCompatActivity {
                 .edit()
                 .putBoolean(getString(R.string.LOGGEDIN), true)
                 .commit();
+
+        LoginTask task = new LoginTask();
+        task.execute(new String[]{LOGGING+"&user="+ userId +"&pwd=" + pwd});
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        /**
+         * This is doInBackground method.
+         * @param urls
+         * @return responce of the method.
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to download the list of cars, Reason: "
+                            + e.getMessage();
+                }
+                finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+        /**
+         * This is onPostExecute method.
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            //parsing the json
+            String[] resultSplit = result.split("\"result\": \"");
+            resultSplit = resultSplit[1].split("\", \"");
+            // Something wrong with the network or the URL.
+            if (resultSplit[0].equals("fail")) {
+                Toast.makeText(getApplicationContext(), "Incorrect Username or Password", Toast.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+            else if(resultSplit[0].equals("success")) {
+                loginSuccess();
+            }
+        }
+    }
+
+    public void loginSuccess(){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
     }
+
 
     public void signUp(View view) {
         Intent k = new Intent(this, SignUpActivity.class);
